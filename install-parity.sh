000fc6bdad0880ae3a443e08bc70147fbc895f87
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2016-11-04 18:13:29 +0100

    Update install-parity.sh

--- install-parity.sh
+++ install-parity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PARITY_DEB_URL=https://github.com/ethcore/parity/releases/download/v1.3.9/parity_1.3.9-0_amd64.deb
+PARITY_DEB_URL=http://d1h4xl4cr1h0mo.cloudfront.net/v1.3.10/x86_64-unknown-linux-gnu/parity_1.3.10_amd64.deb
                
 
 
