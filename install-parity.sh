741b0222776cf9cffa56f910601433f42efec1d9
Arkadiy Paronyan <arkady.paronyan@gmail.com>
2017-01-19 16:35:37 +0100

    Update install-parity.sh

--- install-parity.sh
+++ install-parity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PARITY_DEB_URL=http://d1h4xl4cr1h0mo.cloudfront.net/v1.4.9/x86_64-unknown-linux-gnu/parity_1.4.9_amd64.deb
+PARITY_DEB_URL=http://d1h4xl4cr1h0mo.cloudfront.net/v1.5.0/x86_64-unknown-linux-gnu/parity_1.5.0_amd64.deb
                
 
 
