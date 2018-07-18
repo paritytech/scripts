72a25c8a42a21144f0ee3388abdac00f7f81f669
felix <felix@parity.io>
2018-07-18 17:28:18 +0200

    beta and stable no longer deducted from filename, sha256 check, install binaries for mac

--- get-parity.sh
+++ get-parity.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Copyright 2017 Parity Technologies (UK) Ltd.
+
+## Update this with any new relase!
+VERSION_STABLE="1.11.7"
+VERSION_BETA="2.0.0"
+##
+
 RELEASE="beta"
 ARCH=$(uname -m)
 VANITY_SERVICE_URL="https://vanity-service.parity.io/parity-binaries?architecture=$ARCH&format=markdown"
@@ -31,38 +37,37 @@ get_package() {
 		LOOKUP_URL="$VANITY_SERVICE_URL&os=$PKG&version=$RELEASE"
 	fi
 
-	if [ "$PKG" = "linux" ]; then
 	MD=$(curl -Ss ${LOOKUP_URL} | grep -v sha256 | grep " \[parity\]")
-		DOWNLOAD_FILE=$(echo $MD | grep -oE 'https://[^)]+')
-	fi
-
-	if [ "$PKG" = "darwin" ] ; then
-		MD=$(curl -Ss ${LOOKUP_URL} | grep -v sha256 | grep '\.pkg' )
-		DOWNLOAD_FILE=$(echo $MD | grep -oE 'https://[^)]+')
-	fi
+	DOWNLOAD_FILE=$(echo $MD | grep -oE 'https://[^)]+')
 }
 
 check_upgrade() {
 
-	if [ -x /usr/bin/parity ] ; then
-		OLD_VERSION=$(parity --version | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | tr -d 'v')
-	else
+  # Determin new Version 
+  case "$RELEASE" in
+    "beta")  NEW_VERSION=$VERSION_BETA
+        ;;
+    "stable") NEW_VERSION=$VERSION_STABLE
+        ;;
+    *) NEW_VERSION=$(echo $DOWNLOAD_FILE | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | tr -d 'v')   
+        ;;
+  esac
+
+  # Determin old (installed) Version 
+  parity_bin=$(which parity)
+
+	if [ -z $parity_bin ] ; then
 		OLD_VERSION="0.0.0"
-	fi
-
-	if [ "$PKG" = "linux" ] ; then
-		NEW_VERSION=$(curl -Ss $LOOKUP_URL | grep -oE '_[0-9]+\.[0-9]+\.[0-9]+_' | tr -d '_' | tail -n1)
-	fi
-
-	if [ "$PKG" = "darwin" ] ; then
-		NEW_VERSION=$(echo $DOWNLOAD_FILE | grep -oE '_[0-9]+\.[0-9]+\.[0-9]+_' | tr -d '_' | tail -n1)
+	else
+		OLD_VERSION=$(parity --version | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | tr -d 'v')
 	fi
 
 	if [ "$NEW_VERSION" = "$OLD_VERSION" ] ; then
 		echo "Parity $NEW_VERSION already installed"
 		exit 1
 	fi
-	if  version_gt "$NEW_VERSION" "$OLD_VERSION"  ; then
+
+  if  version_gt "$NEW_VERSION" "$OLD_VERSION"  ; then
 		echo "Upgrading parity from $OLD_VERSION to $NEW_VERSION"
 	else
 		echo "Existing version of parity: $OLD_VERSION is newer than the version you attempting to install: $NEW_VERSION"
@@ -70,22 +75,18 @@ check_upgrade() {
 	fi
 }
 
-
 install() {
   TMPDIR=$(mktemp -d) && cd $TMPDIR
 	curl -Ss -O $DOWNLOAD_FILE
+  check_sha256
 
 	if [ "$PKG" = "linux" ] ; then
-	    sudo cp $TMPDIR/parity /usr/bin
-		sudo chmod +x /usr/bin/parity
+	  sudo cp $TMPDIR/parity /usr/bin && sudo chmod +x /usr/bin/parity
 	fi
 
 	if [ "$PKG" = "darwin" ] ; then
-		NAME=$(basename $DOWNLOAD_FILE)
-		sudo /usr/sbin/installer -pkg $TMPDIR/$NAME -target / && \
-		sudo ln -sF /Applications/Parity\ Ethereum.app/Contents/MacOS/parity /usr/local/bin/parity
+	  sudo cp $TMPDIR/parity /usr/local/bin && sudo chmod +x /usr/local/bin/parity
 	fi
-
 }
 
 
@@ -95,17 +96,52 @@ version_gt() {
 
 
 help() {
-
 	echo "Usage is: -r --release [ stable / beta / nightly ]"
-
 }
 
-# curl installed? 
+
+check_sha256() {
+  # how to check for sha256?
+  SHA256_CHECK=$(which sha256sum 2> /dev/null)
+  if [[ -z $SHA256_CHECK ]] ; then
+    # no sha256sum? try with rhash ...
+    SHA256_CHECK=$(which rhash 2> /dev/null)
+    SHA256_CHECK="$SHA256_CHECK --sha256"
+  fi
+
+  # see if we can call the binary to calculate sha256 sums
+  if ! ($SHA256_CHECK --version &> /dev/null) then
+    echo "Unable to check SHA256 checksum, please install sha256sum or rhash binary"
+    cleanup
+    exit 1
+  fi
+
+  # $SHA256_CHECK $TMPDIR/$DOWNLOAD_FILE 
+  IS_CHECKSUM=$($SHA256_CHECK $TMPDIR/parity | awk '{print $1}')
+  MUST_CHECKSUM=$(curl -sS $LOOKUP_URL | grep ' \[parity\]' | awk '{print $NF'})
+  # debug # echo -e "is checksum:\t $IS_CHECKSUM"
+  # debug # echo -e "must checksum:\t $MUST_CHECKSUM"
+  if [[ $IS_CHECKSUM != $MUST_CHECKSUM ]]; then
+    echo "SHA256 Checksum missmatch, abording installation"
+    cleanup
+    exit 1 
+  fi
+}
+
+cleanup() {
+  rm $TMPDIR/*
+  rmdir $TMPDIR
+}
+
+## MAIN ##
+
+## curl installed? 
 which curl &> /dev/null 
 if [[ $? -ne 0 ]] ; then
     echo '"curl" binary not found, please install and retry'
     exit 1
 fi
+##
 
 while [ "$1" != "" ]; do
 	case $1 in
@@ -118,7 +154,6 @@ while [ "$1" != "" ]; do
 	shift
 	done
 
-
 	echo "Release selected is: $RELEASE"
 
 check_os
@@ -127,3 +162,4 @@ if [ "$RELEASE" != "nightly" ] ; then
 	check_upgrade
 fi
 install
+cleanup
