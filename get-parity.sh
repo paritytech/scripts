04f09b413af5421876737a0d173094a20c68d4e8
Afri Schoedon <5chdn@users.noreply.github.com>
2018-10-11 22:37:59 +0200

    Release Parity 2.0.7 and 2.1.2

--- get-parity.sh
+++ get-parity.sh
@@ -2,8 +2,8 @@
 # Copyright 2015-2018 Parity Technologies (UK) Ltd.
 
 ## Update this with any new relase!
-VERSION_STABLE="2.0.6"
-VERSION_BETA="2.1.1"
+VERSION_STABLE="2.0.7"
+VERSION_BETA="2.1.2"
 ##
 
 RELEASE="beta"
