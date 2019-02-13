b277244831f57f98db2a085f89a0582fc834c7cf
Afri Schoedon <5chdn@users.noreply.github.com>
2019-02-13 15:40:06 +0100

    Update get-parity.sh

--- get-parity.sh
+++ get-parity.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Copyright 2015-2018 Parity Technologies (UK) Ltd.
+# Copyright 2015-2019 Parity Technologies (UK) Ltd.
 
 ## Update this with any new relase!
-VERSION_STABLE="2.2.9"
-VERSION_BETA="2.3.2"
+VERSION_STABLE="2.2.10"
+VERSION_BETA="2.3.3"
 ##
 
 RELEASE="beta"
