b57319ec5996fee222490b515f4c3b7438525634
Sočik <47772477+soc1c@users.noreply.github.com>
2019-03-20 12:28:18 +0100

    Update get-parity.sh

--- get-parity.sh
+++ get-parity.sh
@@ -2,8 +2,8 @@
 # Copyright 2015-2019 Parity Technologies (UK) Ltd.
 
 ## Update this with any new relase!
-VERSION_STABLE="2.3.6"
-VERSION_BETA="2.4.1"
+VERSION_STABLE="2.3.7"
+VERSION_BETA="2.4.2"
 ##
 
 RELEASE="beta"
