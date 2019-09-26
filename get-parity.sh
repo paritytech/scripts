5702052a5971758dc1b4105b44a5053d34b17c31
Martin Pugh <pugh@s3kr.it>
2019-09-26 17:57:09 +0200

    Update get-parity.sh

--- get-parity.sh
+++ get-parity.sh
@@ -2,8 +2,8 @@
 # Copyright 2015-2019 Parity Technologies (UK) Ltd.
 
 ## Update this with any new relase!
-VERSION_STABLE="2.5.8"
-VERSION_BETA="2.6.3"
+VERSION_STABLE="2.5.9"
+VERSION_BETA="2.6.4"
 ##
 
 RELEASE="beta"
