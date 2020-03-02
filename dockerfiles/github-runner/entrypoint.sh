3896af37c004a19c887436ee3443beff56119f9b
Martin Pugh <pugh@s3kr.it>
2020-03-02 19:58:50 +0100

    Add kubernetes yaml, several changes to entrypoint.sh

--- dockerfiles/github-runner/entrypoint.sh
+++ dockerfiles/github-runner/entrypoint.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 export RUNNER_ALLOW_RUNASROOT=1
 
-./config.sh --unattended --token "$RUNNER_TOKEN" --url "$REPO_URL"
+get_registration_token(){
+  token="$(curl \
+    -X POST \
+    -H "Authorization: token $ADMIN_TOKEN" \
+    "https://api.github.com/repos/$REPO/actions/runners/registration-token" | \
+    jq -r .token
+  )"
+  if [ "$RUNNER_TOKEN" = "null" ]; then
+    echo "[!] Something went wrong generating registration token."
+    exit
+  fi
+  echo "$token"
+}
+
+RUNNER_TOKEN=$(get_registration_token)
+
+# Is this even considered a sane thing to do?
+ADMIN_TOKEN=""
+
+./config.sh --unattended --token "$RUNNER_TOKEN" --url "https://github.com/$REPO"
+
+# blank the runner token before starting the runner
+RUNNER_TOKEN=""
 
 ./run.sh
+
