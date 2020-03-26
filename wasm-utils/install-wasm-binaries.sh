9bbbb03117ceb03217fb5556b1d9d99511e9c14d
Denis P <denis.pisarev@parity.io>
2020-03-26 12:33:25 +0100

    fix padding

--- wasm-utils/install-wasm-binaries.sh
+++ wasm-utils/install-wasm-binaries.sh
@@ -36,41 +36,41 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	elif [ -f /etc/debian_version ]; then
 		echo "Ubuntu/Debian Linux detected."
 
-    function install_external_source {
-        set -euv
+		function install_external_source {
+			set -euv
 
-        if ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v tar)" ]; then
-            echo "Installing missing OS packages."
-            $MAKE_ME_ROOT apt update
-            $MAKE_ME_ROOT apt install -y curl jq tar
-        fi
+			if ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v tar)" ]; then
+				echo "Installing missing OS packages."
+				$MAKE_ME_ROOT apt update
+				$MAKE_ME_ROOT apt install -y curl jq tar
+			fi
 
-        BUILD_NUM=$(curl -s https://storage.googleapis.com/wasm-llvm/builds/linux/lkgr.json | jq -r '.build')
-        if [ -z ${BUILD_NUM+x} ]; then
-            echo "Could not fetch the latest build number."
-            exit 1
-        fi
+			BUILD_NUM=$(curl -s https://storage.googleapis.com/wasm-llvm/builds/linux/lkgr.json | jq -r '.build')
+			if [ -z ${BUILD_NUM+x} ]; then
+				echo "Could not fetch the latest build number."
+				exit 1
+			fi
 
-        tmp=$(mktemp -d)
-        pushd "$tmp" >/dev/null
-        echo "Downloading wasm-binaries.tbz2"
-        curl -L -o wasm-binaries.tbz2 "https://storage.googleapis.com/wasm-llvm/builds/linux/$BUILD_NUM/wasm-binaries.tbz2"
+			tmp=$(mktemp -d)
+			pushd "$tmp" >/dev/null
+			echo "Downloading wasm-binaries.tbz2"
+			curl -L -o wasm-binaries.tbz2 "https://storage.googleapis.com/wasm-llvm/builds/linux/$BUILD_NUM/wasm-binaries.tbz2"
 
-        declare -a binaries=("wasm-opt") # Default binaries
-        if [ "$#" -ne 0 ]; then
-            echo "Installing selected binaries."
-            binaries=("$@")
-        else
-            echo "Installing default binaries."
-        fi
+			declare -a binaries=("wasm-opt") # Default binaries
+			if [ "$#" -ne 0 ]; then
+				echo "Installing selected binaries."
+				binaries=("$@")
+			else
+				echo "Installing default binaries."
+			fi
 
-        for bin in "${binaries[@]}"; do
-            echo "Installing $bin into ~/.cargo/bin"
-            tar -xvjf "wasm-binaries.tbz2 wasm-install/bin/$bin" >/dev/null
-            cp -f "wasm-install/bin/$bin" ~/.cargo/bin/
-        done
-        popd >/dev/null
-    }
+			for bin in "${binaries[@]}"; do
+				echo "Installing $bin into ~/.cargo/bin"
+				tar -xvjf "wasm-binaries.tbz2 wasm-install/bin/$bin" >/dev/null
+				cp -f "wasm-install/bin/$bin" ~/.cargo/bin/
+			done
+			popd >/dev/null
+		}
 
 		if apt-cache show binaryen >/dev/null 2>&1; then
 			set -e
