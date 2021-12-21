1117e76a306bec651ec7f5ccf68c33a6fee55a97
alvicsam <alvicsam@gmail.com>
2021-12-21 12:13:56 +0100

    Add new functions, refactor file reading

--- dockerfiles/benchmarks/check_bench_result.py
+++ dockerfiles/benchmarks/check_bench_result.py
@@ -32,18 +32,24 @@ except IndexError:
     sys.exit(1)
 
 try:
-    treshold = os.environ['THRESHOLD']
+    threshold = os.environ['THRESHOLD']
 except KeyError:
-    treshold = 20
+    threshold = 20
 
 prometheus_client = PrometheusConnect(url=prometheus_url, disable_ssl=True)
 github_client = Github(github_token)
 
 def benchmark_last(project,benchmark):
-    """Get latest benchmark result from Victoria Metrics"""
+    """
+    Get latest benchmark result from Victoria Metrics
+    Returns "-1" if result not found
+    """
     query = f"last_over_time(parity_benchmark_common_result_ns{{project=\"{project}\",benchmark=\"{benchmark}\"}}[1y])"
     query_result = prometheus_client.custom_query(query=query)
-    last_benchmark_result = int(query_result[0]['value'][1])
+    if len(query_result) > 0:
+        last_benchmark_result = int(query_result[0]['value'][1])
+    else:
+        last_benchmark_result = -1
     return last_benchmark_result
 
 def create_github_issue(benchmarks_list,github_org_repo):
@@ -55,41 +61,55 @@ def create_github_issue(benchmarks_list,github_org_repo):
     issue_text = f"""
 ## Benchmarks have regressions
 
-Threshold: {treshold}%
+Threshold: {threshold}%
 
 These benchmarks have regressions:
 
-| Benchmark name | Previous result [{last_sha[:8]}]({commit_url}{last_sha}) | Current result [{current_sha[:8]}]({commit_url}{current_sha}) | Ratio |
+| Benchmark name | Previous result [{last_sha[:8]}]({commit_url}{last_sha}) (ns/iter) | Current result [{current_sha[:8]}]({commit_url}{current_sha}) (ns/iter) | Difference (%) |
 |---|---|---|---|
 {benchmarks_mstring}
                  """
     github_issue = repo.create_issue(title="[benchmarks] Regression detected", body=issue_text)
     return github_issue
 
+def get_name_value(line):
+    """Get benchmark name and result from a text line"""
+    name = line.split()[1]
+    value = int(line.split()[4])
+    return name,value
+
+def check_line_valid(line):
+    """
+    Check that line can be transformed to a list with 8 values
+    Expects line "test <benchmark_name_with_underscores> ... bench:  <bench_result> ns/iter (+/- <bench_result_difference_between_max_min>)"
+    """
+    if len(line.split()) != 8:
+        print("Data has wrong format")
+        sys.exit(1)
+    pass
+
+def difference_p(first,second):
+    """Calculate difference between 2 values in percent"""
+    return round(abs(first * 100 / second - 100))
+
 if __name__ == "__main__":
-    output_file = open(results_file, 'r')
-    bench_results = output_file.readlines()
-    output_file.close()
     benchmarks_with_regression = []
-    for result in bench_results:
-        if not "test" in result:
-            continue
-        benchmark_name = result.split()[1]
-        benchmark_current_value = int(result.split()[4])
-        try:
+    with open(results_file, 'r') as file_handle:
+        for result in file_handle:
+            # Skipping lines without benchmark results
+            if not "test" in result:
+                continue
+            check_line_valid(result)
+            benchmark_name,benchmark_current_value = get_name_value(result)
             benchmark_last_value = benchmark_last(github_repo,benchmark_name)
-        except IndexError:
-            print(benchmark_name,"doesn't have data, skipping")
-            continue
-        if benchmark_current_value > benchmark_last_value:
-            ratio = benchmark_current_value * 100 / benchmark_last_value
-            subt = round(abs(100 - ratio))
-            if subt > treshold:
-                regression = True
-                string_to_add = f"| {benchmark_name} | {benchmark_last_value} ns/iter | {benchmark_current_value} ns/iter | {subt}% |"
-                benchmarks_with_regression.append(string_to_add)
-            else:
-                regression = False
+            if benchmark_last_value == -1:
+                print(benchmark_name,"is new and doesn't have any data yet, skipping")
+                continue
+            if benchmark_current_value > benchmark_last_value:
+                diff = difference_p(benchmark_current_value,benchmark_last_value)
+                if diff > threshold:
+                    string_to_add = f"| {benchmark_name} | {benchmark_last_value} ns/iter | {benchmark_current_value} ns/iter | {diff}% |"
+                    benchmarks_with_regression.append(string_to_add)
     if len(benchmarks_with_regression) > 0:
         print("Regression found, creating GitHub issue")
         issue = create_github_issue(benchmarks_with_regression,github_repo_full)
