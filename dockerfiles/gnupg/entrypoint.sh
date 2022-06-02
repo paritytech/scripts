4bfa18baa9e67c0a2a2e64bff7f054d87ba64bcd
Wilfried Kopp <wilfried@parity.io>
2022-06-02 21:01:27 +0200

    WIP That may be THE ONE :)

--- dockerfiles/gnupg/entrypoint.sh
+++ dockerfiles/gnupg/entrypoint.sh
@@ -3,10 +3,19 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+
+if [ -d "/home/user" ]; then
+    useradd --shell /bin/bash -u $USER_ID -o -c "" -M user
+  else
+    useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+fi
+echo "User 'user' created"
 
 export HOME=/home/user
 mkdir -p $HOME/.gnupg
-chown user:user $HOME/.gnupg
+chown -R user:user $HOME/.gnupg
 chmod 700 $HOME/.gnupg
+
+echo before chroot
+whoami
 exec chroot --userspec=user / sh -c "cd ${HOME}; $@"
--- dockerfiles/gnupg/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export HOME=/home/user
-mkdir -p $HOME/.gnupg
-chown user:user $HOME/.gnupg
-chmod 700 $HOME/.gnupg
-exec chroot --userspec=user / sh -c "cd ${HOME}; $@"
