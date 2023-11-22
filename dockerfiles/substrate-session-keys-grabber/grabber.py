15f04c24240efbbd10df4f77367c8ba80a06ca02
Bulat Saifullin <bulat@parity.io>
2023-11-22 15:55:59 +0400

    fix beefy key support (#610)
    Signed-off-by: BulatSaif <bulat@parity.io>

--- dockerfiles/substrate-session-keys-grabber/grabber.py
+++ dockerfiles/substrate-session-keys-grabber/grabber.py
@@ -16,8 +16,8 @@ def parse_session_key(dir):
   # variants of key prefixes in the right order
   key_formats = (
     ['6772616e', '62616265', '696d6f6e', '70617261', '61756469'], # validator keys (gran,babe,imon,para,audi)
-    ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469'], # validator keys (gran,babe,imon,para,asgn,audi)
     ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469', '62656566'], # validator keys (gran,babe,imon,para,asgn,audi,beef)
+    ['6772616e', '62616265', '696d6f6e', '70617261', '6173676e', '61756469'], # validator keys (gran,babe,imon,para,asgn,audi)
     ['61757261'] # collator keys (aura)
     )
   possible_prefixes = list(set([j for i in key_formats for j in i]))
