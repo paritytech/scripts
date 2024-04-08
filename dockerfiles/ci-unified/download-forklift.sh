03d023615620e8cf519e2615d80d59efd4a81749
Evgeny Snitko <evgeny@parity.io>
2024-04-08 19:13:50 +0400

    forklift for ci-unified

---
+++ dockerfiles/ci-unified/download-forklift.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+DL_PATH="$HOME/.forklift4"
+VERSION="latest"
+
+while getopts "p:v:" flag; do
+  case $flag in
+    v) # version
+    VERSION=$OPTARG
+    ;;
+    p) # download path
+    DL_PATH=$OPTARG
+    ;;
+    \?)
+    echo "invalid option '$flag'"
+    exit 1
+    ;;
+  esac
+done
+
+
+echo "Downloading forklift $VERSION to $DL_PATH"
+
+
+RELEASE_URL="https://api.github.com/repos/paritytech/forklift/releases/tags/$VERSION"
+
+if [ $VERSION="latest" ]
+ then
+  RELEASE_URL="https://api.github.com/repos/paritytech/forklift/releases/latest"
+fi
+
+
+RELEASE=`curl -s $RELEASE_URL`
+ASSET=`jq '.assets[] | select(.name | endswith("linux_amd64"))' <<< "$RELEASE"`
+
+ASSET_NAME=`jq -r '.name' <<< "$ASSET"`
+ASSET_URL=`jq -r '.browser_download_url' <<< "$ASSET"`
+
+mkdir -p $DL_PATH
+curl -s -o $DL_PATH/$ASSET_NAME -L $ASSET_URL
+cp -r $DL_PATH/$ASSET_NAME $DL_PATH/forklift
+
+chmod +x $DL_PATH/$ASSET_NAME
+chmod +x $DL_PATH/forklift
